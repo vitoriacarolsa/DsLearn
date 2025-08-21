9cd62263555b43b7922c26e5247164e57b42d8f3
vitoriacarolsa <vitoriacarolinesantos338@gmail.com>
2025-08-20 23:26:22 -0300

    Offer

--- src/main/java/com/devsuperior/dslearn/entities/Course.java
+++ src/main/java/com/devsuperior/dslearn/entities/Course.java
@@ -2,11 +2,21 @@ package com.devsuperior.dslearn.entities;
 
 import jakarta.persistence.*;
 
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Objects;
 
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.OneToMany;
+import jakarta.persistence.Table;
+
 @Entity
-@Table(name= "tb_course")
+@Table(name = "tb_course")
 public class Course {
+
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
@@ -14,9 +24,14 @@ public class Course {
     private String imgUri;
     private String imgGrayUri;
 
-    public Course() {}
+    @OneToMany(mappedBy = "course")
+    private List<Offer> offers = new ArrayList<>();
+
+    public Course() {
+    }
 
     public Course(Long id, String name, String imgUri, String imgGrayUri) {
+        super();
         this.id = id;
         this.name = name;
         this.imgUri = imgUri;
@@ -55,16 +70,24 @@ public class Course {
         this.imgGrayUri = imgGrayUri;
     }
 
-    @Override
-    public boolean equals(Object o) {
-        if (this == o) return true;
-        if (o == null || getClass() != o.getClass()) return false;
-        Course course = (Course) o;
-        return Objects.equals(id, course.id);
+    public List<Offer> getOffers() {
+        return offers;
     }
 
     @Override
     public int hashCode() {
-        return Objects.hashCode(id);
+        return Objects.hash(id);
+    }
+
+    @Override
+    public boolean equals(Object obj) {
+        if (this == obj)
+            return true;
+        if (obj == null)
+            return false;
+        if (getClass() != obj.getClass())
+            return false;
+        Course other = (Course) obj;
+        return Objects.equals(id, other.id);
     }
 }
